805d2b94d29486ca8597a8456afc847eb78d8122
Salandora <salandora@gmail.com>
2024-05-23 00:13:36 +0200

    [Fix]: Fixed merge issue

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/util/QuadTransformers.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/util/QuadTransformers.java
@@ -1,6 +1,8 @@
 package net.p3pp3rf1y.sophisticatedstorage.client.util;
 
 import com.mojang.math.Transformation;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
@@ -42,22 +44,26 @@ public class QuadTransformers {
 	}
 
     public static RenderContext.QuadTransform applying(Transformation transform) {
-        if (transform.isIdentity())
+		if (transform.equals(Transformation.identity()))
             return quad -> true;
+
+		Matrix4f matrix = transform.getMatrix();
+		Matrix3f normalMatrix = new Matrix3f(matrix).invert().transpose();
 
         return quad -> {
             for (int i = 0; i < 4; i++) {
-                Vector4f pos = new Vector4f(quad.x(i), quad.y(i), quad.z(i), 1);
-				transform.transformPosition(pos);
+				Vector4f pos = new Vector4f(quad.x(i), quad.y(i), quad.z(i), 1).mul(transform.getMatrix());
                 pos.div(pos.w);
                 quad.pos(i, pos.x(), pos.y(), pos.z());
             }
 
             for (int i = 0; i < 4; i++) {
                 if (quad.hasNormal(i)) {
-					Vector3f normal = new Vector3f(quad.normalX(i), quad.normalY(i), quad.normalZ(i));
-					transform.transformNormal(normal);
-                    quad.normal(i, normal);
+					quad.normal(i,
+							new Vector3f(quad.normalX(i), quad.normalY(i), quad.normalZ(i))
+									.mul(normalMatrix)
+									.normalize()
+					);
                 }
             }
             return true;
