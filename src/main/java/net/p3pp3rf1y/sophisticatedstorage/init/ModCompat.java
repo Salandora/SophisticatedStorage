fd694ba71e34c6ba033f6432f050a519d04d43ed
Salandora <salandora@gmail.com>
2024-10-12 12:51:10 +0200

    [Fix]: Compatibility registry was instancing the compat modules to early, therefore making all optional mods required.

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModCompat.java
@@ -1,10 +1,10 @@
 package net.p3pp3rf1y.sophisticatedstorage.init;
 
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.chipped.ChippedCompat;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.litematica.LitematicaCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
-import net.p3pp3rf1y.sophisticatedstorage.compat.chipped.ChippedCompat;
-import net.p3pp3rf1y.sophisticatedstorage.compat.litematica.LitematicaCompat;
 import net.p3pp3rf1y.sophisticatedstorage.compat.mkb.ModernKeyBindingCompat;
 import net.p3pp3rf1y.sophisticatedstorage.compat.sodium.SodiumCompat;
 public class ModCompat {
@@ -15,10 +15,10 @@ public class ModCompat {
 	public static final String MKB = "mkb";
 
 	public static void register() {
-		//CompatRegistry.registerCompat(new CompatInfo(CompatModIds.QUARK, null), new QuarkCompat());
-		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.CHIPPED, null), ChippedCompat::new);
-		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.LITEMATICA, null), LitematicaCompat::new);
-		CompatRegistry.registerCompat(new CompatInfo(SODIUM, CompatRegistry.fromSpec(">=0.4.9 <0.5")), SodiumCompat::new);
-		CompatRegistry.registerCompat(new CompatInfo(MKB, null), ModernKeyBindingCompat::new);
+		//CompatRegistry.registerCompat(new CompatInfo(CompatModIds.QUARK, null), () -> new QuarkCompat());
+		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.CHIPPED, null), () -> new ChippedCompat());
+		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.LITEMATICA, null), () -> new LitematicaCompat());
+		CompatRegistry.registerCompat(new CompatInfo(SODIUM, CompatRegistry.fromSpec(">=0.4.9 <0.5")), () -> new SodiumCompat());
+		CompatRegistry.registerCompat(new CompatInfo(MKB, null), () -> new ModernKeyBindingCompat());
 	}
 }
