eed09ec287f9753e89dfd6eed397486bfeb757cb
Salandora <salandora@gmail.com>
2025-01-03 01:19:23 +0100

    [Feat]: Added setModelData method in preparation for storage in motion

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/BarrelBakedModelBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/client/render/BarrelBakedModelBase.java
@@ -191,6 +191,10 @@ public abstract class BarrelBakedModelBase implements BakedModel, CustomParticle
 		return false;
 	}
 
+	public void setModelData(ModelData data) {
+		this.modelData = data;
+	}
+
 	@Override
 	public void emitBlockQuads(BlockAndTintGetter blockView, BlockState state, BlockPos pos, Supplier<RandomSource> randomSupplier, RenderContext context) {
 		modelData = getModelData(blockView, pos, state, ModelData.EMPTY);
@@ -200,12 +204,6 @@ public abstract class BarrelBakedModelBase implements BakedModel, CustomParticle
 	@Override
 	public void emitItemQuads(ItemStack stack, Supplier<RandomSource> randomSupplier, RenderContext context) {
 		modelData = null;
-		// TODO: Still needed?
-		/*if (this.barrelItemOverrides != null) {
-			// need this here because of REI's fast entry rendering feature
-			// it is doing batched rendering and this collides with how item overrides are implemented here
-			this.barrelItemOverrides.resolve(this, stack, null, null, 0);
-		}*/
 		BakedModel.super.emitItemQuads(stack, randomSupplier, context);
 	}
 
