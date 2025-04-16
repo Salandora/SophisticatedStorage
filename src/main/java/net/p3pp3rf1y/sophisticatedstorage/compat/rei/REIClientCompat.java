76be9ebed92a611705c3e79cc22957b8baf529c1
Salandora <salandora@gmail.com>
2025-04-16 17:24:58 +0200

    [Fix]: Fix a bug with REI deleting items when using auto fill

--- src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/rei/REIClientCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/rei/REIClientCompat.java
@@ -4,8 +4,10 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.Rect2i;
 import net.p3pp3rf1y.sophisticatedcore.compat.rei.REISettingsGhostIngredientHandler;
 import net.p3pp3rf1y.sophisticatedcore.compat.rei.REIStorageGhostIngredientHandler;
+import net.p3pp3rf1y.sophisticatedcore.compat.rei.SophisticatedTransferHandler;
 import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageScreen;
 import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageSettingsScreen;
+import net.p3pp3rf1y.sophisticatedstorage.common.gui.StorageContainerMenu;
 import net.p3pp3rf1y.sophisticatedstorage.compat.jei.DyeRecipesMaker;
 import net.p3pp3rf1y.sophisticatedstorage.compat.jei.FlatBarrelRecipesMaker;
 import net.p3pp3rf1y.sophisticatedstorage.compat.jei.ShulkerBoxFromChestRecipesMaker;
@@ -17,6 +19,7 @@ import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ExclusionZones;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.Display;
 import me.shedaniel.rei.api.common.util.EntryStacks;
@@ -46,6 +49,11 @@ public class REIClientCompat implements REIClientPlugin {
 
         zones.register(StorageSettingsScreen.class, screen -> screen.getSettingsTabControl().getTabRectangles().stream().map(r -> new Rectangle(r.getX(), r.getY(), r.getWidth(), r.getHeight())).toList());
     }
+
+	@Override
+	public void registerTransferHandlers(TransferHandlerRegistry registry) {
+		registry.register(SophisticatedTransferHandler.crafting(StorageContainerMenu.class));
+	}
 
 	@Override
 	public void registerCategories(CategoryRegistry registry) {
